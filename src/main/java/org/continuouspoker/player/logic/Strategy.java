99853297d115ddc477e7b59e30593ef08caf584e
Nico <5772511+Death111@users.noreply.github.com>
2023-02-24 10:22:20 +0100

    bet min bet

--- src/main/java/org/continuouspoker/player/logic/Strategy.java
+++ src/main/java/org/continuouspoker/player/logic/Strategy.java
@@ -1,13 +1,28 @@
 package org.continuouspoker.player.logic;
 
-import org.continuouspoker.player.model.Bet;
-import org.continuouspoker.player.model.Table;
+import org.continuouspoker.player.model.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Strategy {
 
+   Map<Rank, Integer> rankMap = new HashMap<>();
+   Map<Suit, Integer> rankSuit = new HashMap<>();
+
    public Bet decide(final Table table) {
       System.out.println(table);
-      return new Bet().bet(0);
+
+      List<Card> communityCards = table.getCommunityCards();
+      Player me = table.getPlayers().get(table.getActivePlayer());
+
+
+      return new Bet().bet(table.getMinimumBet());
+   }
+
+   public int countPairs(List<Card> cards){
+     return 0;
    }
 
 }
--- src/test/java/org/continuouspoker/player/logic/StrategyTest.java
+++ src/test/java/org/continuouspoker/player/logic/StrategyTest.java
@@ -1,14 +1,17 @@
 package org.continuouspoker.player.logic;
 
+import org.continuouspoker.player.model.*;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-import org.continuouspoker.player.model.Bet;
-import org.continuouspoker.player.model.Table;
+
+import java.util.Arrays;
+import java.util.List;
 
 class StrategyTest {
 
     Strategy strategy = new Strategy();
+
     @Test
     void decide() {
         // ARRANGE
@@ -19,5 +22,38 @@ class StrategyTest {
 
         // ASSERT7
         assertEquals(bet.getBet(), 0);
+    }
+
+    @Test
+    void decide2() {
+        // ARRANGE
+        Table table = new Table();
+
+
+        // ACT
+        Bet bet = strategy.decide(table);
+
+        // ASSERT7
+        assertEquals(bet.getBet(), 0);
+    }
+
+    @Test
+    void testCountPairs() {
+        List<Card> cards = Arrays.asList(
+                cardFrom(Rank._2, Suit.CLUBS),
+                cardFrom(Rank._2, Suit.CLUBS)
+        );
+
+        int pairs = strategy.countPairs(cards);
+
+        assertEquals(pairs, 1);
+    }
+
+
+    private Card cardFrom(Rank rank, Suit suit){
+        Card card = new Card();
+        card.rank(rank);
+        card.suit(suit);
+        return card;
     }
 }
