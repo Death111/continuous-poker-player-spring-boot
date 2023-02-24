fa1728f37fcb62cdb21b53787226f452fef541a7
Nico <5772511+Death111@users.noreply.github.com>
2023-02-24 10:22:21 +0100

    countPairs

--- src/main/java/org/continuouspoker/player/logic/Strategy.java
+++ src/main/java/org/continuouspoker/player/logic/Strategy.java
@@ -8,21 +8,63 @@ import java.util.Map;
 
 public class Strategy {
 
-   Map<Rank, Integer> rankMap = new HashMap<>();
-   Map<Suit, Integer> rankSuit = new HashMap<>();
+    Map<Rank, Integer> rankMap = new HashMap<>();
+    Map<Suit, Integer> rankSuit = new HashMap<>();
 
-   public Bet decide(final Table table) {
-      System.out.println(table);
+    public Bet decide(final Table table) {
+        System.out.println(table);
 
-      List<Card> communityCards = table.getCommunityCards();
-      Player me = table.getPlayers().get(table.getActivePlayer());
+        List<Card> communityCards = table.getCommunityCards();
+        Player me = table.getPlayers().get(table.getActivePlayer());
 
+        List<Card> cards = communityCards;
+        cards.addAll(me.getCards());
 
-      return new Bet().bet(table.getMinimumBet());
-   }
+        HashMap<Rank, Integer> rankIntegerHashMap = countRanks(cards);
 
-   public int countPairs(List<Card> cards){
-     return 0;
-   }
+        HashMap<Suit, Integer> suitIntegerHashMap = countSuits(cards);
 
+        int highestRankCount = 0;
+        for (Rank rank : rankIntegerHashMap.keySet()) {
+            Integer rankCount = rankIntegerHashMap.get(rank);
+            if (rankCount > highestRankCount) highestRankCount = rankCount;
+        }
+        
+        int bet = 0;
+        if (highestRankCount >= 2) {
+            bet = highestRankCount * 10;
+        }
+
+        return new Bet().bet(bet);
+    }
+
+    public HashMap<Rank, Integer> countRanks(List<Card> cards) {
+        HashMap<Rank, Integer> rankIntegerHashMap = new HashMap<>();
+
+        for (Card card : cards) {
+            Integer rankCount = rankIntegerHashMap.get(card.getRank());
+            if (rankCount == null) {
+                rankIntegerHashMap.put(card.getRank(), 1);
+            } else {
+                rankIntegerHashMap.put(card.getRank(), rankCount + 1);
+            }
+        }
+
+        return rankIntegerHashMap;
+    }
+
+    public HashMap<Suit, Integer> countSuits(List<Card> cards) {
+        HashMap<Suit, Integer> rankIntegerHashMap = new HashMap<>();
+
+        for (Card card : cards) {
+            Integer rankCount = rankIntegerHashMap.get(card.getSuit());
+            if (rankCount == null) {
+                rankIntegerHashMap.put(card.getSuit(), 1);
+            } else {
+                rankIntegerHashMap.put(card.getSuit(), rankCount + 1);
+            }
+        }
+
+        return rankIntegerHashMap;
+    }
 }
--- src/test/java/org/continuouspoker/player/logic/StrategyTest.java
+++ src/test/java/org/continuouspoker/player/logic/StrategyTest.java
@@ -7,7 +7,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Disabled
 class StrategyTest {
@@ -27,30 +29,28 @@ class StrategyTest {
     }
 
     @Test
-    void decide2() {
-        // ARRANGE
-        Table table = new Table();
-
-
-        // ACT
-        Bet bet = strategy.decide(table);
-
-        // ASSERT7
-        assertEquals(bet.getBet(), 0);
-    }
-
-    @Test
-    void testCountPairs() {
+    void testCountRanks() {
         List<Card> cards = Arrays.asList(
                 cardFrom(Rank._2, Suit.CLUBS),
                 cardFrom(Rank._2, Suit.CLUBS)
         );
 
-        int pairs = strategy.countPairs(cards);
+        HashMap<Rank, Integer> countedRanks = strategy.countRanks(cards);
 
-        assertEquals(pairs, 1);
+        assertEquals(2, countedRanks.get(Rank._2));
     }
 
+    @Test
+    void testCountSuites() {
+        List<Card> cards = Arrays.asList(
+                cardFrom(Rank._2, Suit.CLUBS),
+                cardFrom(Rank._2, Suit.CLUBS)
+        );
+
+        HashMap<Suit, Integer> countedRanks = strategy.countSuits(cards);
+
+        assertEquals(2, countedRanks.get(Suit.CLUBS));
+    }
 
     private Card cardFrom(Rank rank, Suit suit){
         Card card = new Card();
